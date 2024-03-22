597a77e6c3d3700c1c58c3135965889cc1d5a1ec
TigreBleu74 <tigrebleu74460@gmail.com>
2024-03-22 16:44:19 +0100

    Backend: GET /api/users/:userId

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -4,7 +4,28 @@ import { User, Product, Bid } from '../orm/index.js'
 const router = express.Router()
 
 router.get('/api/users/:userId', async (req, res) => {
-  res.status(600).send()
+  const info: User | null = await User.findOne({
+    where: { id: req.params['userId'] },
+    include: [
+      { model: Product, as: 'products' },
+      { model: Bid, as: 'bids' }
+    ]
+  });
+
+  if (!info)
+    res.status(404).json({ 'error': 'User not found' });
+
+  else
+    res.status(200).json(
+      {
+        "id": info.id,
+        "username": info.username,
+        "email": info.email,
+        "admin": info.admin,
+        "products": info.products,
+        "bids": info.bids
+      }
+    );
 })
 
 export default router
