7d30dbdb73b8dcbccfc6a311c3a22c765c1405f7
Enzo Monchanin <nzosifou@gmail.com>
2024-03-29 17:15:33 +0100

    GET list products and product id

--- backend/routes/product.ts
+++ backend/routes/product.ts
@@ -6,17 +6,58 @@ import { getDetails } from '../validators/index.js'
 const router = express.Router()
 
 router.get('/api/products', async (req, res, next) => {
-  res.status(600).send()
-})
+  const products: Product[] | null = await Product.findAll({
+    include: [
+      { model: User, as: 'seller', attributes: ['id', 'username'] },
+      { model: Bid, as: 'bids', attributes: ['id', 'price', 'date'] }
+    ]
+  });
+
+  if(!products) return res.status(404).json({ 'error': 'No products found' });
+
+  const listProducts = products.map(product => ({
+    "id": product.id,
+    "name": product.name,
+    "description": product.description,
+    "category": product.category,
+    "originalPrice": product.originalPrice,
+    "pictureUrl": product.pictureUrl,
+    "endDate": product.endDate,
+    "seller": product.seller,
+    "bids": product.bids
+  }));
+
+  res.status(200).json(listProducts);
+});
 
 router.get('/api/products/:productId', async (req, res) => {
-  res.status(600).send()
+  const product: Product | null = await Product.findOne({
+    where: { id: req.params['productId'] },
+    include: [
+      { model: User, as: 'seller', attributes: ['id', 'username'] },
+      { model: Bid, as: 'bids', attributes: ['id', 'price', 'date'] }
+    ]
+  });
+
+  if(!product) return res.status(404).json( { "error": "Product not found" } );
+
+  res.status(200).json({
+    "id": product.id,
+    "name": product.name,
+    "description": product.description,
+    "category": product.category,
+    "originalPrice": product.originalPrice,
+    "pictureUrl": product.pictureUrl,
+    "endDate": product.endDate,
+    "seller": product.seller,
+    "bids": product.bids
+  });
 })
 
 // You can use the authMiddleware with req.user.id to authenticate your endpoint ;)
 
-router.post('/api/products', (req, res) => {
-  res.status(600).send()
+router.post('/api/products', authMiddleware, (req, res) => {
+
 })
 
 router.put('/api/products/:productId', async (req, res) => {
