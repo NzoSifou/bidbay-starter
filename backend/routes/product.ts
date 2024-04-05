c9c5b7a891c402cbdac9122c99336b505f3da134
TigreBleu74 <tigrebleu74460@gmail.com>
2024-04-05 16:20:22 +0200

    PUT /api/products/:productId

--- backend/routes/product.ts
+++ backend/routes/product.ts
@@ -56,7 +56,7 @@ router.get('/api/products/:productId', async (req, res) => {
 
 // You can use the authMiddleware with req.user.id to authenticate your endpoint ;)
 
-router.post('/api/products', authMiddleware, async (req: Request & {user?: Token}, res) => {
+router.post('/api/products', authMiddleware, async (req: Request & { user?: Token }, res) => {
   const name: string | undefined = req.body['name'];
   const description: string | undefined = req.body['description'];
   const pictureUrl: string | undefined = req.body['pictureUrl'];
@@ -88,8 +88,55 @@ router.post('/api/products', authMiddleware, async (req: Request & {user?: Token
   );
 })
 
-router.put('/api/products/:productId', async (req, res) => {
-  res.status(600).send()
+router.put('/api/products/:productId', authMiddleware, async (req: Request & { user?: Token }, res) => {
+  const name: string | undefined = req.body['name'];
+  const description: string | undefined = req.body['description'];
+  const pictureUrl: string | undefined = req.body['pictureUrl'];
+  const category: string | undefined = req.body['category'];
+  const originalPrice: number = + req.body['originalPrice'];
+  const endDate: string | undefined = req.body['endDate'];
+  const sellerId: string | undefined = req.body['sellerId'];
+
+  if(!name || !endDate || isNaN(Date.parse(endDate)))
+    return res.status(400).json(
+      {
+        'error': 'Invalid or missing fields',
+        'details': [ 'name', 'endDate' ]
+      }
+    );
+
+  const product: Product | null = await Product.findOne({
+    where: { id: req.params['productId'] }
+  });
+
+  if(!product)
+    return res.status(404).json(
+      {
+        'error': 'Product not found'
+      }
+    );
+
+  if(product.sellerId !== req.user!.id && !req.user!.admin)
+    return res.status(403).json(
+      {
+        'error': 'Forbidden'
+      }
+    );
+
+  await product.update({ id: req.params['productId'], name, description, category, originalPrice, pictureUrl, endDate, sellerId })
+
+  res.status(200).json(
+    {
+      id: product.id,
+      name: product.name,
+      description: product.description,
+      category: product.category,
+      originalPrice: product.originalPrice,
+      pictureUrl: product.pictureUrl,
+      endDate: product.endDate,
+      sellerId: product.sellerId,
+    }
+  );
 })
 
 router.delete('/api/products/:productId', async (req, res) => {
